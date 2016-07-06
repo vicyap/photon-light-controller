43990fb964df357d13b808c431530e9c81615c80
Victor Yap <victor.yap@utexas.edu>
2016-07-05 23:01:49 -0500

    implemented Heartbeat singleton class

--- rgbstrip.ino
+++ rgbstrip.ino
@@ -1,21 +1,65 @@
+#pragma SPARK_NO_PREPROCESSOR
 
-int brightness;
+#include "application.h"
 
+class Heartbeat {
+public:
+    Heartbeat(Heartbeat const&) = delete;
+    void operator=(Heartbeat const&) = delete;
+
+public:
+    void start() {
+        _timer.start();
+    }
+
+    void stop() {
+        _timer.stop();
+    }
+
+    static Heartbeat& instance() {
+        static Heartbeat inst;
+        RGB.control(true);
+        RGB.color(255, 0, 0);
+        RGB.brightness(0);
+        return inst;
+    }
+
+private:
+    static Heartbeat* _inst;
+    Heartbeat() :
+    _period(1000),
+    _isOn(false),
+    _brightness(128),
+    _timer(_period, &Heartbeat::_toggleHeartbeat, *this)
+    {}
+
+    void _toggleHeartbeat() {
+        if (_isOn) {
+            RGB.brightness(0);
+            _isOn = false;
+        }
+        else {
+            RGB.brightness(_brightness);
+            _isOn = true;
+        }
+    }
+
+    int _period;
+    bool _isOn;
+    int _brightness;
+    Timer _timer;
+};
+
+/* VARIABLES */
+
+/* FUNCTIONS */
+
+/* SETUP */
 void setup() {
-
-RGB.control(true);
-RGB.color(255, 0, 255);
-brightness = 0;
-
+    Heartbeat::instance().start();
 }
 
+/* LOOP */
 void loop() {
-
-
-brightness = (brightness + 10) & 0xFF;
-RGB.brightness(brightness);
-
-delay(50);
-
 
 }
