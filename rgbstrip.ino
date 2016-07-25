8d78eec73157546055fd6deabef7edd5e6396079
Victor Yap <victor.yap@utexas.edu>
2016-07-25 02:54:15 -0500

    flashing with the music, red=lo, green=mid, blue=hi, on/off demo

--- rgbstrip.ino
+++ rgbstrip.ino
@@ -22,6 +22,10 @@ Msgeq7 msgeq(A0, D3, D4);
 void setup() {
     Heartbeat::instance().start();
 
+    red.strobeFreq(10);
+    green.strobeFreq(10);
+    blue.strobeFreq(10);
+
     Serial.begin(9600);
     Serial.println("Hello World!");
 }
@@ -34,5 +38,24 @@ void loop() {
     Serial.printf("%4d %4d %4d %4d %4d %4d %4d\n",
         s[0], s[1], s[2], s[3], s[4], s[5], s[6]);
 
-    delay(100);
+    if (s[0] > 3000 || s[1] > 3000) {
+        red.on();
+    }
+    else {
+        red.off();
+    }
+
+    if (s[2] > 3000 || s[3] > 3000 || s[4] > 3000) {
+        green.on();
+    }
+    else {
+        green.off();
+    }
+
+    if (s[5] > 3000 || s[6] > 3000) {
+        blue.on();
+    }
+    else {
+        blue.off();
+    }
 }
