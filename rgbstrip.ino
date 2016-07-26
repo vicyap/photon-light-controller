6f2761518ddb276e4b24992e606f61fa5b8cd83d
Victor Yap <victor.yap@utexas.edu>
2016-07-26 01:00:05 -0500

    RGMW with music demo, needs polish

--- rgbstrip.ino
+++ rgbstrip.ino
@@ -14,7 +14,12 @@ Led red(D0);
 Led green(D1);
 Led blue(D2);
 
-Msgeq7 msgeq(A0, D3, D4);
+Led white(D3);
+
+Msgeq7 msgeq(A0, D4, D5);
+
+#define THRESH 0.33
+#define SUM_THRESH 20000
 
 /* FUNCTIONS */
 
@@ -22,9 +27,7 @@ Msgeq7 msgeq(A0, D3, D4);
 void setup() {
     Heartbeat::instance().start();
 
-    red.strobeFreq(10);
-    green.strobeFreq(10);
-    blue.strobeFreq(10);
+    white.strobeFreq(10);
 
     Serial.begin(9600);
     Serial.println("Hello World!");
@@ -38,24 +41,52 @@ void loop() {
     Serial.printf("%4d %4d %4d %4d %4d %4d %4d\n",
         s[0], s[1], s[2], s[3], s[4], s[5], s[6]);
 
-    if (s[0] > 3000 || s[1] > 3000) {
-        red.on();
+    std::array<double, 7> n;
+
+    double sum = 0;
+    for (const auto &t : s) {
+        sum += t;
+    }
+    for (unsigned i = 0; i < 7; ++i) {
+        n[i] = (s[i] / sum);
+    }
+
+    Serial.printf("%1.3f %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n",
+        n[0], n[1], n[2], n[3], n[4], n[5], n[6]);
+
+    if (n[0] > THRESH || n[1] > THRESH) {
+        int duty = (int) ((n[0] + n[1]) * 255);
+        red.setDuty(duty);
     }
     else {
         red.off();
     }
 
-    if (s[2] > 3000 || s[3] > 3000 || s[4] > 3000) {
-        green.on();
+    if (n[2] > THRESH || n[3] > THRESH) {
+        int duty = (int) ((n[2] + n[3]) * 255);
+        green.setDuty(duty);
     }
     else {
         green.off();
     }
 
-    if (s[5] > 3000 || s[6] > 3000) {
-        blue.on();
+    if (n[4] > THRESH || n[5] > THRESH || n[6] > THRESH) {
+        int duty = (int) ((n[4] + n[5] + n[6]) * 255);
+        blue.setDuty(duty);
     }
     else {
         blue.off();
     }
+
+    if (sum > SUM_THRESH) {
+        white.on();
+        red.off();
+        green.off();
+        blue.off();
+    }
+    else {
+        white.off();
+    }
+    Serial.printf("\n");
+
 }
